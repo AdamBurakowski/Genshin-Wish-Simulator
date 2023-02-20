5b98341612074884ebd2bef51a4325fa591d106c
Adam Burakowski <125091826+AdamBurakowski@users.noreply.github.com>
2023-02-20 19:44:19 +0100

    Update wish.py

--- wish.py
+++ wish.py
@@ -17,15 +17,15 @@ def characterRoll(banner, standard, fifty_fifty):
 
 # This function has all the logic responsible for choosing the outcome of users wish
 
-def roll(wish, banner5, standard5, banner4, standard4, fifty_fifty5, fifty_fifty4, garbage, soft_pity, count):
+def roll(wish, banner5, standard5, banner4, standard4, fifty_fifty5, fifty_fifty4, garbage, soft_pity, pity):
     if wish <= 6 + soft_pity:
         banner5, standard5, fifty_fifty5 = characterRoll(banner5, standard5, fifty_fifty5)
-        #print(f"You rolled a 5-star at {count%90}") # Writes how deep into pity you was while pulling a 5-star (if you do a lot of wishes i reccomend to comment it because there'll be a lot of printing...)
+        pity = 0
     elif wish <= 51 + soft_pity and wish > 6 + soft_pity:
         banner4, standard4, fifty_fifty4 = characterRoll(banner4, standard4, fifty_fifty4)
     else:
         garbage += 1
-    return banner5, standard5, banner4, standard4, fifty_fifty5, fifty_fifty4, garbage
+    return banner5, standard5, banner4, standard4, fifty_fifty5, fifty_fifty4, garbage, pity
 
 
 def main():
@@ -35,7 +35,9 @@ def main():
     five_star_fifty_fifty = False
     four_star_fifty_fifty = False
     pity = 1
-    soft_pity = 0 # Soft pity is a theory popular among Genshin players. It postulate that if you reach 75 pity, chances to drop 5-star (slightly) increase. Most of 5-star pulls occur between 75 to 85 wishes, so it seems like a reasonable theory, but it was never confirmed by the devs.
+    soft_pity = 0
+    # Soft pity is a theory popular among Genshin players. It postulate that if you reach 75 pity, chances to drop 5-star (slightly) increase.
+    # Most of 5-star pulls occur between 75 to 85 wishes, so it seems like a reasonable theory, but it was never confirmed by the devs.
     banner_five_star_count = 0
     standard_five_star_count = 0
     banner_four_star_count = 0
@@ -45,7 +47,6 @@ def main():
     for wishes in range(num_of_wishes):
         if pity % 90 == 0 and pity != 0:
             banner_five_star_count, standard_five_star_count, five_star_fifty_fifty = characterRoll(banner_five_star_count, standard_five_star_count, five_star_fifty_fifty)
-            #print("you rolled guaranteed 5-star") 
             pity = 0
         elif pity % 10 == 0 and pity != 0:
             banner_four_star_count, standard_four_star_count, four_star_fifty_fifty = characterRoll(banner_four_star_count, standard_four_star_count, four_star_fifty_fifty)
@@ -55,7 +56,7 @@ def main():
             else:
                 soft_pity = 0
             wish = r.randint(1,1000)
-            banner_five_star_count, standard_five_star_count, banner_four_star_count, standard_four_star_count, five_star_fifty_fifty, four_star_fifty_fifty, garbage_count = roll(wish, banner_five_star_count, standard_five_star_count, banner_four_star_count, standard_four_star_count, five_star_fifty_fifty, four_star_fifty_fifty, garbage_count, soft_pity, wishes)
+            banner_five_star_count, standard_five_star_count, banner_four_star_count, standard_four_star_count, five_star_fifty_fifty, four_star_fifty_fifty, garbage_count, pity = roll(wish, banner_five_star_count, standard_five_star_count, banner_four_star_count, standard_four_star_count, five_star_fifty_fifty, four_star_fifty_fifty, garbage_count, soft_pity, pity)
         pity += 1
 
     print(f"You rolled {num_of_wishes} wishes and got:\n{banner_five_star_count} banner 5-stars\n{standard_five_star_count} standard 5-stars\n{banner_four_star_count} banner 4-stars\n{standard_four_star_count} standard 4-stars\n{garbage_count} garbage\n\n")
